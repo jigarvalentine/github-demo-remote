7101795cc4194219f7b3c0553a5b9dc041b9135b
jigar thakkar <jigar.thakkar@persistent.com>
2023-10-07 02:37:55 +0530

    merged local with remote

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
   public static void main (String[] args) {
+<<<<<<< Updated upstream
     System.out.println("Hello World remote again!");
+=======
+    System.out.println("Hello World local!");
+>>>>>>> Stashed changes
   }
 }
